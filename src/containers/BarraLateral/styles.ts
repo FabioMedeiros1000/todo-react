bbf826d4b9bdc1bc8a78f81fc7f2f2b141175dc6
FabioMedeiros1000 <fabio.leandro.medeiros@gmail.com>
2024-12-02 16:50:32 -0300

    :iphone: creating the responsive part

--- src/containers/BarraLateral/styles.ts
+++ src/containers/BarraLateral/styles.ts
@@ -11,4 +11,9 @@ export const Filtros = styled.div`
   grid-template-columns: 1fr 1fr;
   grid-gap: 8px;
   margin-top: 16px;
+
+  @media (max-width: 768px) {
+    grid-template-columns: 1fr;
+    grid-gap: 0;
+  }
 `
--- src/styles/index.ts
+++ src/styles/index.ts
@@ -14,6 +14,10 @@ const EstiloGlobal = createGlobalStyle`
 export const Container = styled.div`
   display: grid;
   grid-template-columns: 224px auto;
+
+  @media (max-width: 768px) {
+    grid-template-columns: 150px auto;
+  }
 `
 
 export const MainContainer = styled.main`
